1e462151d1470669639a2cb6c980683ecadd83a5
Josmar Dias <josmarnet@gmail.com>
2018-02-02 02:27:41 -0200

    refactor(exchange-sdk): compile whole api config instead of cherry picking values

--- src/exchange-sdk/sdk.js
+++ src/exchange-sdk/sdk.js
@@ -1,5 +1,6 @@
 import { fetchThis, getResult } from 'fetch-this'
 import Handlebars from 'handlebars'
+import flow from 'lodash.flow'
 
 Handlebars.registerHelper('upper', (str) => {
   if (!str || typeof str !== 'string') {
@@ -8,18 +9,19 @@ Handlebars.registerHelper('upper', (str) => {
   return str.toUpperCase()
 })
 
-export const getPrice = async (exchange, payload) => {
-  const compile = (template) => {
-    if (!template) return ''
-    return Handlebars.compile(template)(payload)
-  }
+// TODO: move this logic to a new package
+// perf note: "stringify/compile/parse" performs better than "recursive compile"
+//  the last one performs worse as object gets larger or deeper
+const compileObj = (obj, payload) => (
+  flow([
+    JSON.stringify,
+    (_) => Handlebars.compile(_)(payload),
+    JSON.parse,
+  ])(obj)
+)
 
-  const data = {
-    fetch: {
-      url: compile(exchange.api.fetch.url),
-    },
-    result: compile(exchange.api.result),
-  }
+export const getPrice = async (exchange, payload) => {
+  const data = compileObj(exchange.api, payload)
 
   const response = await fetchThis(data)
   const value = await getResult(response, data)
