ef3bb8b3e87b6d76fe0b2c4bcf770fce0119426c
Josmar Dias <josmarnet@gmail.com>
2018-02-05 01:28:55 -0200

    refactor(exchange-sdk): upgrade fetch-this, adjust breaking changes

--- src/exchange-sdk/sdk.js
+++ src/exchange-sdk/sdk.js
@@ -4,8 +4,8 @@ import { expandJson } from 'expand-json'
 export const getPrice = async (exchange, payload) => {
   const data = expandJson(exchange.api, payload)
 
-  const response = await fetchThis(data)
-  const value = await getResult(response, data)
+  const response = await fetchThis(data.fetch)
+  const value = await getResult(response, data.result)
 
   return value
 }
