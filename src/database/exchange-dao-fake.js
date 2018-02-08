706700108e160e995d4ff17c9cb95621053794a3
Josmar Dias <josmarnet@gmail.com>
2018-02-08 01:20:51 -0200

    feat: add database layer to retrieve exchanges metadata

---
+++ src/database/exchange-dao-fake.js
@@ -0,0 +1,7 @@
+let data
+
+export const loadFake = (_data) => data = _data
+
+export const getAllExchanges = async () => data.exchanges
+
+export const getAllCurrencies = async () => data.currencies
---
+++ src/database/exchange-dao.js
@@ -0,0 +1,5 @@
+import data from '../../data.json'
+
+export const getAllExchanges = async () => data.exchanges
+
+export const getAllCurrencies = async () => data.currencies
