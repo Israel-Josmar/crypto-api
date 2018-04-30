a2b2bca4177835a9894c8f4ff69b90e531b5d30e
Josmar Dias <josmarnet@gmail.com>
2018-04-30 01:25:48 -0300

    refactor: remove "minimal artifacts" approach, too much complexity

--- src/index-express.js
+++ src/index-express.js
@@ -1,31 +1,34 @@
 import express from 'express'
 import { getDashboard } from './app/dashboard'
-import { getAll, loadAll } from './exchange/data-access/prices-dao'
-import { populateCache } from './exchange/services/cripto-prices'
-import { getAllExchanges, getAllCurrencies, getChosenExchangeId } from './exchange/data-access/exchange-dao'
-import { getPrice } from './exchange/vendor-api/exchanges-sdk'
+import * as pricesCache from './exchange/data-access/prices-dao'
+import * as pricesService from './exchange/services/cripto-prices'
+import * as exchangeDAO from './exchange/data-access/exchange-dao'
+import * as exchangeSDK from './exchange/vendor-api/exchanges-sdk'
 
 const server = express()
 const port = 3001
 
 server.get('/dashboard', async (req, res) => {
-  try {
-    {
-      // move this service call to a background job
-      const pricesCache = { loadAll }
-      const exchangeDAO = { getAllExchanges, getAllCurrencies }
-      const exchangeSDK = { getPrice }
-      await populateCache({ pricesCache, exchangeDAO, exchangeSDK })
-    }
+  // TODO: move this logic to a background job
+  await simulateBackgroundJobAlreadyRunned()
 
-    const pricesCache = { getAll }
-    const chosenExchangeId = await getChosenExchangeId()
+  try {
+    const chosenExchangeId = await exchangeDAO.getChosenExchangeId()
     const dashboard = await getDashboard(pricesCache, chosenExchangeId)
     return res.send(dashboard)
   } finally {
     res.status(500).end()
   }
 })
+
+const simulateBackgroundJobAlreadyRunned = async () => {
+  // prepopulate cache
+  await pricesService.populateCache({ pricesCache, exchangeDAO, exchangeSDK })
+
+  // TODO: on a real background job
+  //  prepopulate profit cache with some predefined amounts
+  // ex.: await profitService.populateProfitCache({ ..., amount: predefinedAmount  })
+}
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}!`) // eslint-disable-line
