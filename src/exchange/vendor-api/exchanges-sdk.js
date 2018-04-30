66d3f7c5534696cf7adc322a5ef975bd20ccffd2
Josmar Dias <josmarnet@gmail.com>
2018-04-30 02:36:51 -0300

    feat: transform book formats to a known structure

--- src/exchange/vendor-api/exchanges-sdk.js
+++ src/exchange/vendor-api/exchanges-sdk.js
@@ -1,11 +1,39 @@
 import { fetchByConfig } from './fetch-util'
 import { expandJson } from 'expand-json'
+import get from 'lodash.get'
+
+const transformList = {
+  /*
+   * [[ $price, $amount ], ...]
+  */
+  fromArrayOfArray: (arr) => arr.map(([price, amount]) => ({ price, amount })),
+  /*
+   * [{ price: $price, amount: $amount }], ...]
+  */
+  fromArrayOfObject: (arr) => arr,
+}
 
 export const getBook = async (exchange, payload) => {
   const config = expandJson(exchange.api, payload)
   const result = await fetchByConfig(config)
 
-  return result
+  const {
+    bids: bidsConfig,
+    asks: asksConfig,
+  } = config.transform
+
+  const rawBids = get(result, bidsConfig.path)
+  const rawAsks = get(result, asksConfig.path)
+
+  const transformBids = transformList[bidsConfig.method]
+  const transformAsks = transformList[asksConfig.method]
+
+  const book = {
+    bids: transformBids(rawBids),
+    asks: transformAsks(rawAsks),
+  }
+
+  return book
 }
 
 // FIXME: remove as soon as finish chaning ticker to booking
