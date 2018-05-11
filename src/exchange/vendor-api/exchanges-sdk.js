18becc96bdd33698d53ce8a3c4ad30e2efed9325
Josmar Dias <josmarnet@gmail.com>
2018-05-10 22:12:11 -0300

    feat: getBook on exchanges sdk

--- src/exchange/vendor-api/exchanges-sdk.js
+++ src/exchange/vendor-api/exchanges-sdk.js
@@ -1,9 +1,12 @@
 import { fetchByConfig } from './fetch-util'
 import { expandJson } from 'expand-json'
 
-export const getPrice = async (exchange, payload) => {
+export const getBook = async (exchange, payload) => {
   const config = expandJson(exchange.api, payload)
   const result = await fetchByConfig(config)
 
   return result
 }
+
+// FIXME: remove as soon as finish chaning ticker to booking
+export const getPrice = getBook
