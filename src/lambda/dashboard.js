0a37e9317ab3f50365b9acb8626dd384aa011597
Josmar Dias <josmarnet@gmail.com>
2018-11-21 22:48:36 -0200

    refactor: move handlers to a lambda folder

--- src/lambda/dashboard.js
+++ src/lambda/dashboard.js
@@ -4,9 +4,9 @@ global.fetch = require('node-fetch')
 
 import middy from 'middy'
 import { cors } from 'middy/middlewares'
-import { getDashboard } from './app/dashboard'
+import { getDashboard } from '../app/dashboard'
 
-const dashboardHandler = async (event, context, callback) => {
+const dashboard = async (event, context, callback) => {
   const userAmount = event.queryStringParameters.amount
   const currency = event.queryStringParameters.currency
 
@@ -20,5 +20,5 @@ const dashboardHandler = async (event, context, callback) => {
   callback(null, response)
 }
 
-export const dashboard = middy(dashboardHandler)
+export default middy(dashboard)
   .use(cors())
